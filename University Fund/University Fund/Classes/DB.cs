09d2f2bb4b52b3ccb17e2a0655111769854035e4
felocSsS <71203042+felocSsS@users.noreply.github.com>
2021-09-10 16:15:22 +0300

    Add files via upload

---
+++ University Fund/University Fund/Classes/DB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University_Fund.DBFolder;
+
+namespace University_Fund.Classes
+{
+    class DB
+    {
+        public static Entities1 db;
+    }
+}
